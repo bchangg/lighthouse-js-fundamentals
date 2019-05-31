122126c5f9dd312e566fa61b820895d5e8749388
Brian Chang <changbhc@gmail.com>
2019-05-31 04:16:51 +0000

    Working on ternary statements

--- tinkering.js
+++ tinkering.js
@@ -1,28 +1,21 @@
 /*
- * Programming Quiz: Murder Mystery (3-4)
+ * Programming Quiz - Navigating the Food Chain (3-8)
+ *
+ * Use a series of ternary operator to set the category to one of the following:
+ *   - "herbivore" if an animal eats plants
+ *   - "carnivore" if an animal eats animals
+ *   - "omnivore" if an animal eats plants and animals
+ *   - undefined if an animal doesn't eat plants or animals
+ *
+ * Notes
+ *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+ *   - `if` statements aren't allowed ;-)
  */
 
-// change the value of `room` and `suspect` to test your code
-var room = "dining room";
-var suspect = "Mr. Parkes";
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = false;
+var eatsAnimals = true;
 
-var weapon = "";
-var solved = false;
+var category = (eatsPlants && eatsAnimals) ? "omnivore" : (eatsPlants ? "herbivore" : (eatsAnimals ? "carnivore" : undefined));
 
-if (suspect == "Mr. Parkes" && room == "dining room") {
-  weapon = "knife";
-  solved = true;
-} else if (suspect == "Ms. Van Cleve" && room == "gallery") {
-  weapon = "trophy";
-  solved = true;
-} else if (suspect == "Mrs. Sparr" && room == "billiards room") {
-  weapon = "pool stick";
-  solved = true;
-} else if (suspect == "Mr. Kalehoff" && room == "ballroom") {
-  weapon = "poison";
-  solved = true;
-}
-
-if (solved) {
-	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
-}
+console.log(category);
