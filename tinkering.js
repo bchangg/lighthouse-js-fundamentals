5b1162a8898ba203448ae92b1fa6221ee2c1e3e6
Brian Chang <changbhc@gmail.com>
2019-06-01 04:19:39 +0000

    Completed Udacity: Intro to Javascript '99 Bottles of Juice' puzzle in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,18 +1,27 @@
 /*
- * Programming Quiz: JuliaJames (4-1)
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
  */
 
-var x = 1;
+var num = 99;
 
-while (x <= 20) {
-  if ((x % 3 === 0) && (x % 5 === 0)) {
-    console.log("JuliaJames");
-  } else if (x % 3 === 0) {
-    console.log("Julia");
-  } else if (x % 5 === 0) {
-    console.log("James");
+while (num > 0) {
+  if (num === 2) {
+    console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num-1) + " bottle of juice on the wall!");
+  } else if (num > 1) {
+    console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!");
   } else {
-    console.log(x);
+    console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num-1) + " bottles of juice on the wall!");
   }
-  x++;
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+    num--;
 }
