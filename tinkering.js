c3b00a95997a0e25e1285903d5512645278fcfd6
Brian Chang <changbhc@gmail.com>
2019-06-01 04:07:01 +0000

    Completed Udacity: Intro to Javascript 'JuliaJames' puzzle in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,39 +1,18 @@
 /*
- * Programming Quiz: Back to School (3-9)
- *
- * Write a switch statement to set the average salary of a person based on their type of completed education.
- *
+ * Programming Quiz: JuliaJames (4-1)
  */
 
-// change the value of `education` to test your code
-var education = 'a high school diploma';
+var x = 1;
 
-// set the value of this based on a person's education
-var salary = 0;
-
-// your code goes here
-switch (education) {
-  case "no high school diploma":
-    salary = 25636;
-    break;
-  case "a high school diploma":
-    salary = 35256;
-    break;
-  case "an Associate's degree":
-    salary = 41496;
-    break;
-  case "a Bachelor's degree":
-    salary = 59124;
-    break;
-  case "a Master's degree":
-    salary = 69732;
-    break;
-  case "a Professional degree":
-    salary = 89960;
-    break;
-  case "a Doctoral degree":
-    salary = 84396;
-    break;
+while (x <= 20) {
+  if ((x % 3 === 0) && (x % 5 === 0)) {
+    console.log("JuliaJames");
+  } else if (x % 3 === 0) {
+    console.log("Julia");
+  } else if (x % 5 === 0) {
+    console.log("James");
+  } else {
+    console.log(x);
+  }
+  x++;
 }
-
-console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
