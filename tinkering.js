1022ac4d2e3bf24d68a68a58fccdef5ef7b2d6ed
Brian Chang <changbhc@gmail.com>
2019-06-01 04:34:33 +0000

    Completed Udacity: Intro to Javascript 'Changing the Loop' puzzle in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,40 +1,13 @@
 /*
- * Programming Quiz: Countdown, Liftoff! (4-3)
- *
- * Using a while loop, print out the countdown output above.
- *
- * Conditions:
- * Orbiter transfers from ground to internal power (T-50 seconds)
- * Ground launch sequencer is go for auto sequence start (T-31 seconds)
- * Activate launch pad sound suppression system (T-16 seconds)
- * Activate main engine hydrogen burnoff system (T-10 seconds)
- * Main engine start (T-6 seconds)
- * Solid rocket booster ignition and liftoff! (T-0 seconds)
+ * Programming Quiz: Changing the Loop (4-4)
  */
 
-let seconds = 60;
-while (seconds >= 0) {
-  switch (seconds) {
-    case 50:
-      console.log("Orbiter transfers from ground to internal power");
-      break;
-    case 31:
-      console.log("Ground launch sequencer is go for auto sequence start");
-      break;
-    case 16:
-      console.log("Activate launch pad sound suppression system");
-      break;
-    case 10:
-      console.log("Activate main engine hydrogen burnoff system");
-      break;
-    case 6:
-      console.log("Main engine start");
-      break;
-    case 0:
-      console.log("Solid rocket booster ignition and liftoff!");
-      break;
-    default:
-      console.log("T-" + seconds + " seconds");
-  }
-  seconds--;
+// rewrite the while loop as a for loop
+for(let x = 9; x >= 1; x--) {
+  console.log("hello " + x);
 }
+var x = 9;
+// while (x >= 1) {
+//     console.log("hello " + x);
+//     x = x - 1;
+// }
