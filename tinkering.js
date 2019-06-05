1af362db31e789b336bbf642130d0d75bcbda319
Brian Chang <changbhc@gmail.com>
2019-06-05 20:48:22 +0000

    Completed Udacity: Functions (5-4)

--- tinkering.js
+++ tinkering.js
@@ -1,25 +1,14 @@
 /*
- * Programming Quiz: Build A Triangle (5-3)
+ * Programming Quiz: Laugh (5-4)
  */
 
-// creates a line of * for a given length
-function makeLine(length) {
-    var line = "";
-    for (var j = 1; j <= length; j++) {
-        line += "* ";
-    }
-    return line + "\n";
-}
-
-// your code goes here.  Make sure you call makeLine() in your own code.
-function buildTriangle(lines) {
-  let result = ""
-  for(let i = 1; i <= lines; i++) {
-    result += makeLine(i);
+var laugh = function(num) {
+  let result = "";
+  for(let i = 0; i < num; i++) {
+    result += "ha";
   }
+  result += "!";
   return result;
 }
 
-
-// test your code by uncommenting the following line
-console.log(buildTriangle(10));
+console.log(laugh(10));
