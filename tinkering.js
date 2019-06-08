bfd18aea2ce4e54918e728dbe72e5765ad9dd1ae
Brian Chang <changbhc@gmail.com>
2019-06-08 05:16:09 +0000

    Completed Udacity: Arrays Q6-2

--- tinkering.js
+++ tinkering.js
@@ -1,21 +1,8 @@
 /*
- * Programming Quiz: Inline Functions (5-6)
+ * Programming Quiz: UdaciFamily (6-1)
  */
 
-// don't change this code
-function emotions(myString, myFunc) {
-    console.log("I am " + myString + ", " + myFunc(2));
-}
 
-// your code goes here
-// call the emotions function here and pass in an
-// inline function expression
+ let udaciFamily = ["Julia", "James", "Brian"];
 
-emotions("happy", function laugh(num) {
-  let result = "";
-  for(let i = 0; i < num; i++) {
-    result += "ha";
-  }
-  result += "!";
-  return result;
-}); // you can use your laugh function from the previous quizzes
+ console.log(udaciFamily);
