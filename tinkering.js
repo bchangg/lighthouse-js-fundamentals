eb89e7fcc6b28cb5250be67b32ff5392aa8fc2b7
Brian Chang <changbhc@gmail.com>
2019-06-08 07:14:20 +0000

    Completed Udacity: Arrays 6-8

--- tinkering.js
+++ tinkering.js
@@ -1,20 +1,23 @@
 /*
- * Programming Quiz: Joining the Crew (6-6)
+ * Programming Quiz: Another Type of Loop (6-8)
+ *
+ * Use the existing `test` variable and write a `forEach` loop
+ * that adds 100 to each number that is divisible by 3.
+ *
+ * Things to note:
+ *  - you must use an `if` statement to verify code is divisible by 3
+ *  - you can use `console.log` to verify the `test` variable when you're finished looping
  */
 
-var captain = "Mal";
-var second = "Zoe";
-var pilot = "Wash";
-var companion = "Inara";
-var mercenary = "Jayne";
-var mechanic = "Kaylee";
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+  19, 300, 3775, 299, 36, 209, 148, 169, 299,
+  6, 109, 20, 58, 139, 59, 3, 1, 139
+];
 
-var crew = [captain, second, pilot, companion, mercenary, mechanic];
+test.forEach(function(item, index, array) {
+  if (item % 3 === 0) {
+    array[index] += 100;
+  }
+});
 
-var doctor = "Simon";
-var sister = "River";
-var shepherd = "Book";
-
-crew.push(doctor, sister, shepherd);
-
-console.log(crew);
+console.log(test);
