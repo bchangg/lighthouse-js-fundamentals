cb11d0050ef103ce29664ea70f9f439b482ee360
Brian Chang <changbhc@gmail.com>
2019-06-08 05:36:27 +0000

    Completed Udacity: Arrays Q6-5

--- tinkering.js
+++ tinkering.js
@@ -1,14 +1,11 @@
 /*
- * Programming Quiz: Colors of the Rainbow (6-4)
- *
- * Use only the splice() method to modify the rainbow variable:
- *  - remove "Blackberry"
- *  - add "Yellow" and "Green"
- *  - add "Purple"
+ * Programming Quiz: Quidditch Cup (6-5)
  */
 
-var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 
-rainbow.splice(2, 0);
-rainbow.splice(2, 1, "Yellow", "Green");
-rainbow.splice(5, 0, "Purple");
+function hasEnoughPlayers(team) {
+  return team.length >= 7;
+}
+
+console.log(hasEnoughPlayers(team));
