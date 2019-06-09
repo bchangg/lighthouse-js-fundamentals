feae7fcec222c069112b95c654b29d723909cd05
Brian Chang <changbhc@gmail.com>
2019-06-09 17:39:59 +0000

    Completed Looping: lastIndexOf

--- tinkering.js
+++ tinkering.js
@@ -1,15 +1,15 @@
-function range(start, end, step) {
-  let result = [];
-  if ((start === undefined || end === undefined || step === undefined) || (start > end) || (step <= 0)) {
-    return result;
-  } else {
-    for (let i = start; i <= end; i += step) {
-      result.push(i);
+function lastIndexOf(inputArray, value) {
+  let result = -1;
+  inputArray.forEach(function(currentValue, index, workingArray) {
+    if (currentValue === value) {
+      result = index;
     }
-    return result;
-  }
+  });
+  return result;
 }
 
-console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
