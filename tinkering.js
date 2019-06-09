e7d093193f9c11355490b8eb318f851f239186c0
Brian Chang <changbhc@gmail.com>
2019-06-09 18:00:29 +0000

    Begin working on Udacity 7-1

--- tinkering.js
+++ tinkering.js
@@ -1,14 +1,17 @@
-function concat(array1, array2) {
-  let newLength = array1.length + array2.length;
-  let result = array1.slice(0);
-  array2.forEach(function(currentValue) {
-    result.push(currentValue);
-  })
-  result.sort();
-  return result;
-}
+/*
+ * Programming Quiz: Umbrella (7-1)
+ */
 
-console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
-console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
-console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
-console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
+var umbrella = {
+    color: "pink",
+    isOpen: true,
+    open: function() {
+        if (umbrella.isOpen === true) {
+            return "The umbrella is already opened!";
+        } else {
+            umbrella.isOpen = true;
+            return "Julia opens the umbrella!";
+        }
+    },
+    // your code goes here
+};
