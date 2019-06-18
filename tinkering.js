82791a7c07f3289eef8ca14c46e8b2f93364aca9
Brian Chang <changbhc@gmail.com>
2019-06-18 03:54:36 +0000

    Completed Kata 4 in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,13 +1,43 @@
-const numberOfVowels = function(data) {
-  let sum = 0;
-  for(let i = 0; i < data.length; i++) {
-    if(data[i] === 'a' || data[i] === 'e' || data[i] === 'i' || data[i] === 'o' || data[i] === 'u' ) {
-      sum++;
+const instructorWithLongestName = function(instructors) {
+  let index = 0;
+  let longestName = "";
+
+  for (let i = 0; i < instructors.length; i++) {
+    if (instructors[i].name.length > longestName.length) {
+      longestName = instructors[i].name;
+      index = i;
     }
   }
-  return sum;
+  return instructors[index];
 };
 
-console.log(numberOfVowels("orange"));
-console.log(numberOfVowels("lighthouse labs"));
-console.log(numberOfVowels("aeiou"));
+console.log(instructorWithLongestName([{
+    name: "Samuel",
+    course: "iOS"
+  },
+  {
+    name: "Jeremiah",
+    course: "Web"
+  },
+  {
+    name: "Ophilia",
+    course: "Web"
+  },
+  {
+    name: "Donald",
+    course: "Web"
+  }
+]));
+console.log(instructorWithLongestName([{
+    name: "Matthew",
+    course: "Web"
+  },
+  {
+    name: "David",
+    course: "iOS"
+  },
+  {
+    name: "Domascus",
+    course: "Web"
+  }
+]));
