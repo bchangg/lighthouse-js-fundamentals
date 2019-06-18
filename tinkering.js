1567fe1609b7d5afb2392dd497786e232af56640
Brian Chang <changbhc@gmail.com>
2019-06-18 03:36:13 +0000

    Completed loopyLighthouse2.js, completed Kata 1 in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,21 +1,19 @@
-/*
- * Programming Quiz: Facebook Friends (7-5)
- */
-
-let facebookProfile = {
-  name: "Brian",
-  friends: 10,
-  messages: ["hello", "world", "hola"],
-  postMessage: function(message) {
-    this.messages.push(message);
-  },
-  deleteMessage: function(index) {
-    this.messages.splice(index, 1);
-  },
-  addFriend: function() {
-    this.friends++;
-  },
-  removeFriend: function() {
-    this.friends--;
+const sumLargestNumbers = function(data) {
+  if (data.length === 2) {
+    return data[0] + data[1];
+  } else {
+    let largest = data[0];
+    let secondLargest = data[0];
+    for (let i = 0; i < data.length-1; i++) {
+      if(data[i+1] > data[i]) {
+        secondLargest = largest;
+        largest = data[i+1];
+      }
+    }
+    return largest + secondLargest;
   }
-}
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
