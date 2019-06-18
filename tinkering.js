1bed752bb82b26826f50aab9b9bfce77732e5fd9
Brian Chang <changbhc@gmail.com>
2019-06-18 03:41:05 +0000

    Completed Kata 2 in tinkering.js

--- tinkering.js
+++ tinkering.js
@@ -1,19 +1,16 @@
-const sumLargestNumbers = function(data) {
-  if (data.length === 2) {
-    return data[0] + data[1];
-  } else {
-    let largest = data[0];
-    let secondLargest = data[0];
-    for (let i = 0; i < data.length-1; i++) {
-      if(data[i+1] > data[i]) {
-        secondLargest = largest;
-        largest = data[i+1];
-      }
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] % 2 === 1 && condition === "odd") {
+      sum += values[i];
+    } else if (values[i] % 2 === 0 && condition === "even") {
+      sum += values[i];
     }
-    return largest + secondLargest;
   }
+  return sum;
 };
 
-console.log(sumLargestNumbers([1, 10]));
-console.log(sumLargestNumbers([1, 2, 3]));
-console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
